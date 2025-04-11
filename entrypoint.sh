398f3fc47e6af4bc8ed6538d1d2b37029ff95dd8
Xander Smeets <xander@smeets.ee>
2025-04-11 02:54:39 +0200

    Simplify obtaining input file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,8 @@
 echo "::debug::Entrypoint script started."
 echo "::debug::Current working directory: $(pwd)"
 
-# Get the input file from the GitHub Action input or the first argument
-TEX_FILE="${INPUT_TEX_FILE:-$1}"
-
 # Check if the input file is provided
-if [ -z "$TEX_FILE" ]; then
+if [ -z "$INPUT_TEX_FILE" ]; then
   echo "::error file=entrypoint.sh,line=7::No .tex file provided. Please specify the 'tex-file' input."
   exit 1
 fi
