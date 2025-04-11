9585a4a92f6a027dfe49805d39d75afc7caedd95
Xander Smeets <xander@smeets.ee>
2025-04-11 03:40:30 +0200

    Revert input file changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,12 @@
 echo "::debug::Entrypoint script started."
 echo "::debug::Current working directory: $(pwd)"
 
+# Get the input file from the GitHub Action input or the first argument
+TEX_FILE="${INPUT_TEX_FILE:-$1}"
+
 # Check if the input file is provided
-if [ -z "$INPUT_TEX_FILE" ]; then
-  echo "::error file=entrypoint.sh,line=7::No .tex file provided. Please specify the 'tex-file' input."
+if [ -z "$TEX_FILE" ]; then
+  echo "::error file=entrypoint.sh,line=7::No .tex file provided. Please specify the 'tex_file' input."
   exit 1
 fi
 
